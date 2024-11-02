2ed4d43b167f96d1998ef997b31091593c283a56
andrew mofreh <andrewmofreh@gmail.com>
2024-11-02 08:10:50 -0700

    first upload

---
+++ js/main.js
@@ -0,0 +1,25 @@
+console.log("Hello %cfrom the %cother side ", "color: red; font-size:40px", "color: blue; font-size: 50px");
+console.table(["andrew", "mofreh", "adly"]);
+console.error("Error")
+var myname = "andrew"
+console.log("Hello " + myname);
+// string
+console.log(typeof "andrew mofreh");
+// string
+console.log(typeof "5000");
+// string
+console.log(typeof 5000);
+// it's name is array it's type is object
+console.log(typeof [10, 15, 17]);
+// it's name is array it's type is object
+console.log(typeof ["andrew", "mofreh", "adly"]);
+// name is key and andrew is value & it's type is object
+console.log(typeof { name: "andrew", age: 27, });
+// boolen
+console.log(typeof true);
+// boolen
+console.log(typeof false);
+// undefined
+console.log(typeof undefined);
+// object
+console.log(typeof null);
